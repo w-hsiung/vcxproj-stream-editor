b8f1e44e352581ca5b21d0d99d679f3f42115ba5
WH <whsiung.my@gmail.com>
2023-03-13 10:50:41 +0800

    Add Ex1.vcxproj for testing.

--- usage.py
+++ usage.py
@@ -1,7 +1,18 @@
+import vcxproj
 
+
+@vcxproj.coroutine
+def print_project_guid():
+    while True:
+        action, params = yield
+        if action == "start_elem" and params["name"] == "ProjectGuid":
+            action, params = yield
+            assert action == "chars"
+            print("Project GUID is ", params["content"])
 
 
 def main():
+    vcxproj.check_file("Ex1.vcxproj", print_project_guid)
     return
 
 
