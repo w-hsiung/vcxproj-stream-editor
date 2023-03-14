c7fb655229048019326319018064bf46c93c89bb
WH <whsiung.my@gmail.com>
2023-03-14 08:02:27 +0800

    Revert changes to remove_warning_level.

--- usage.py
+++ usage.py
@@ -71,16 +71,11 @@ def remove_warning_level(target):
     while True:
         action, params = yield
         if action == "start_elem" and params["name"] == "WarningLevel":
-            target.send((action, params))
             action, params = yield
             assert action == "chars"
-            print(params["content"])
-            params["content"] = "Level4"
-            target.send((action, params))
             action, params = yield
             assert action == "end_elem"
             assert params["name"] == "WarningLevel"
-            target.send((action, params))
             continue
         target.send((action, params))
 
