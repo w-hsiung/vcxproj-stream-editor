a753347a683f4c8f2661696888c6366a855ed901
WH <whsiung.my@gmail.com>
2023-03-14 16:50:54 +0800

    Remove print_element().

--- usage.py
+++ usage.py
@@ -12,61 +12,6 @@ def print_project_guid():
 
 
 @vcxproj.coroutine
-def print_element():
-    while True:
-        action, params = yield
-        if action == "start_elem" and params["name"] == "ItemDefinitionGroup":
-            tup = params["attrs"]
-            print(params["name"], tup["Condition"])
-            continue
-        if action == "start_elem" and params["name"] == "OutDir":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "IntDir":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "TypeLibraryName":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "PrecompiledHeaderOutputFile":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "AssemblerListingLocation":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "ObjectFileName":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "ProgramDataBaseFileName":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "OutputFile":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-        if action == "start_elem" and params["name"] == "ProgramDatabaseFile":
-            name = params["name"]
-            action, params = yield
-            print(name, params["content"])
-            continue
-
-
-@vcxproj.coroutine
 def remove_warning_level(target):
     while True:
         action, params = yield
