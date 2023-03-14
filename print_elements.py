7b162830efde95296c6203abcdd814732e7fa885
WH <whsiung.my@gmail.com>
2023-03-14 16:49:21 +0800

    Add print_elements.py.

---
+++ print_elements.py
@@ -0,0 +1,98 @@
+import vcxproj
+
+
+@vcxproj.coroutine
+def print_elements():
+    while True:
+        action, params = yield
+        if action == "start_elem" and params["name"] == "ItemDefinitionGroup":
+            tup = params["attrs"]
+            print(params["name"], tup["Condition"])
+            continue
+        if action == "start_elem" and params["name"] == "OutDir":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "IntDir":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "TypeLibraryName":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "PrecompiledHeaderOutputFile":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "AssemblerListingLocation":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "ObjectFileName":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "ProgramDataBaseFileName":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "OutputFile":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+        if action == "start_elem" and params["name"] == "ProgramDatabaseFile":
+            name = params["name"]
+            action, params = yield
+            print(name, params["content"])
+            continue
+
+
+@vcxproj.coroutine
+def remove_warning_level(target):
+    while True:
+        action, params = yield
+        if action == "start_elem" and params["name"] == "WarningLevel":
+            action, params = yield
+            assert action == "chars"
+            action, params = yield
+            assert action == "end_elem"
+            assert params["name"] == "WarningLevel"
+            continue
+        target.send((action, params))
+
+
+@vcxproj.coroutine
+def change_build_settings(target):
+    while True:
+        action, params = yield
+        if action == "start_elem" and params["name"] == "WarningLevel":
+            target.send((action, params))
+            action, params = yield
+            assert action == "chars"
+            print(params["content"])
+            params["content"] = "Level4"
+            target.send((action, params))
+            action, params = yield
+            assert action == "end_elem"
+            assert params["name"] == "WarningLevel"
+            target.send((action, params))
+            continue
+        target.send((action, params))
+
+
+def main():
+    vcxproj.check_file("Ex1.vcxproj", print_elements)
+    return
+
+
+if __name__ == "__main__":
+    main()
